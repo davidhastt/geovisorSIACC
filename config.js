6126ac7d563aec2aa024487e63510df30a6557dd
David millan <davidhastt@gmail.com>
2025-07-14 02:01:07 -0600

    Ahora carga las capas desde un json, incluyendo temas, grupos de capas y capas

--- config.js
+++ config.js
@@ -1,2 +1,8 @@
 const host="localhost";
 const javaPort="8080";
+
+
+//Crea los archivos lsd en QGIS
+//crea los estilos en geoserver
+//crea las capas en geoserver
+    // Ponle descripcion a las capas en geoserver
